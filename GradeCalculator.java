e7789ad6ebd0ee582d7f0af019e73dbc8af5dd0d
raquelrubillo17 <raquelrubillo17.email@example.com>
2025-02-24 10:04:17 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        int score = getStudentScore();
+        String grade = calculateGrade(score);
+        System.out.println("Your Grade is: " + grade);
+    }
+
+    
+    public static int getStudentScore() {
+        Scanner scanner = new Scanner(System.in);
+        int score;
+        
+        while (true) {
+            try {
+                System.out.print("Enter your score: ");
+                score = Integer.parseInt(scanner.nextLine());
+                
+                if (score < 0 || score > 100) {
+                    System.out.println("Please enter a valid score between 0 and 100.");
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a numeric value.");
+            }
+        }
+        
+        scanner.close();
+        return score;
+    }
+
+    
+    public static String calculateGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
